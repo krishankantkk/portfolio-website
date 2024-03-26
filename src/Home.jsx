1363d38410fe493987bef0bd6da83fb37d8d61b8
krishan kant <kantk1508@gmail.com>
2024-03-26 15:31:51 +0530

    new resume

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,29 @@
-
 import Particle from "./Particle";
 export default function Home() {
   return (
     <>
       <Particle />
-      <div
-        className="home flex flex-col items-center justify-center gap-4 h-screen align-center"
-        
-      >
+      <div className="home flex flex-col items-center justify-center gap-4 h-screen align-center">
         <p className="name font-medium text-white text-8xl content-center">
           Krishan <span className="text-sky-500">Kant</span>
         </p>
         <p className="title text-white text-2xl">
-         Aspiring Software Developer & frontend Developer
+          Aspiring Software Developer & frontend Developer
         </p>
         <p className="socials text-white text-3xl flex gap-x-6 mt-2">
           <a href="mailto:kantk1508@gmail.com" target="_blank">
             <i className="fa-solid fa-envelope text-white"></i>
           </a>
           <a
-            href="https://drive.google.com/file/d/1XRLGGQQzmEjVw4iWqTdZV5mRaVPEVbij/view"
+            href="https://drive.google.com/file/d/1qfxbUeKHLgUTWeJwHYKzmI7kN0nRymlH/view?usp=sharing"
             target="_blank"
           >
             <i className="fa-solid fa-file-lines text-white"></i>
           </a>
-          <a href="https://www.linkedin.com/in/krishan-kant-2828aa217/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/krishan-kant-2828aa217/"
+            target="_blank"
+          >
             <i className="fa-brands fa-linkedin text-white"></i>
           </a>
 
